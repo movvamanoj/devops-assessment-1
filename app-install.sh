2b6967023fea0bad534b9da842e533c01a47d9eb
Manojkumar Movva <movvamanoj@gmail.com>
2023-06-07 14:49:10 +0530

    Update app-install.sh
    add install docker on redhat

--- app-install.sh
+++ app-install.sh
@@ -8,29 +8,59 @@ sudo echo '<h1>Hello MANOJKUMAR MOVVA, Your New APP Ready</h1>' | sudo tee /var/
 sudo mkdir /var/www/html/app1
 sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>OneMuthoot - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
 sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
-#########################################################################################################
-# Install Java 8 Development Kit
-    sudo yum install -y java-1.8.0-openjdk-devel
-    
-    # Install Docker dependencies
-    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-    
-    # Add Docker repository
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    
-    # Install Docker
-    sudo yum install -y docker-ce docker-ce-cli containerd.io
-    
-    # Start Docker service
-    sudo systemctl start docker
-    
-    # Enable Docker service to start on boot
-    sudo systemctl enable docker
-    
-    # Add the current user to the docker group
-    sudo usermod -aG docker ec2-user
-    
-    # Pull and run the Docker image from Docker Hub
-    sudo docker run -d -p 8080:8080 movvamanojaws/my-app
-    ########################################################################################################################
+##############amazon_linux###########################################################################################
+# Update the system
+sudo yum update -y
 
+# Install Docker dependencies
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Add Docker repository
+sudo amazon-linux-extras install docker
+
+# Install Docker
+sudo yum install -y docker
+
+# Start Docker service
+sudo service docker start
+
+# Enable Docker service to start on boot
+sudo chkconfig docker on
+
+# Add the current user to the docker group
+sudo usermod -aG docker $(whoami)
+
+# Pull and run the Docker image from Docker Hub
+sudo docker run -d -p 8080:8080 movvamanojaws/my-app
+
+# Verify Docker installation
+docker --version
+###############################
+
+
+######################docker_install_on_redhat######################################
+# Update the system
+#sudo yum update -y
+
+# Install Docker dependencies
+#sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Add Docker repository
+#sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install Docker
+#sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+# Start Docker service
+#sudo systemctl start docker
+
+# Enable Docker service to start on boot
+#sudo systemctl enable docker
+
+# Add the current user to the docker group
+#sudo usermod -aG docker $(whoami)
+# Pull and run the Docker image from Docker Hub
+#sudo docker run -d -p 8080:8080 movvamanojaws/my-app
+# Verify Docker installation
+#docker --version
+#######################################
