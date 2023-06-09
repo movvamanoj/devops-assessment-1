b9f9bfb3636659fddae0a04f0a3066d9d624bddd
Manojkumar Movva <movvamanoj@gmail.com>
2023-06-09 14:18:11 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -37,10 +37,8 @@ install_redhat_packages() {
   sudo yum install -y docker-ce docker-ce-cli containerd.io
   sudo systemctl start docker
   sudo systemctl enable docker
-}
 
 # Function to install Jenkins
-install_jenkins() {
   sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
   sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
   sudo yum install -y jenkins
