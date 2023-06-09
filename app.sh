c993f592c04f82a6716ac0a42bd752a156826fdf
Manojkumar Movva <movvamanoj@gmail.com>
2023-06-09 18:06:47 +0530

    Update app.sh
    eco tags

--- app.sh
+++ app.sh
@@ -3,27 +3,35 @@ set -e
 set -x  # Enable verbose mode
 # Function to install Red Hat packages
 install_redhat_packages() {
-  # Install Red Hat specific packages
-  sudo yum update -y
-  sudo yum install -y wget unzip
-   # Install Java 11
-  sudo yum install -y java-11-openjdk-devel
-  echo "export JAVA11_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc
-  source ~/.bashrc
-  
-  # Install Java 8
-  sudo yum install -y java-1.8.0-openjdk-devel
-  echo "export JAVA8_HOME=/usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
-  source ~/.bashrc
-echo "export DOCKER_JAVA_PATH=usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
-  source ~/.bashrc
-  
+# Install Red Hat specific packages
+sudo yum update -y
+sudo yum install -y wget unzip
+# Install Java 11
+echo "Installing java11..."
+sudo yum install -y java-11-openjdk-devel
+echo "export JAVA11_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc
+source ~/.bashrc
+echo "Installing java11 done..."
+echo "Installing java 8..."
 
-  # Install Java 17
-  sudo yum install -y java-17-openjdk-devel
-  echo "export SONAR_JAVA_PATH=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc
-  source ~/.bashrc
-sleep 10
+# Install Java 8
+sudo yum install -y java-1.8.0-openjdk-devel
+echo "export JAVA8_HOME=/usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
+source ~/.bashrc
+echo "Installing java8 done..."
+
+echo "export DOCKER_JAVA_PATH=usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Installing java17..."
+# Install Java 17
+sudo yum install -y java-17-openjdk-devel
+echo "export SONAR_JAVA_PATH=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc
+source ~/.bashrc
+echo "Installing java17 done..."
+
+echo "Installing Git..."
+
 sudo yum install -y git
 cd ~/
 mkdir github
@@ -38,10 +46,16 @@ USERNAME=$(cat "/home/movvamanoj/gitrepo/gitusername.txt")
 
 # Clone the repository using the Git token and username for authentication
 git clone https://${USERNAME}:${GIT_TOKEN}@github.com/movvamanoj/movvaweb.git
+echo "Git installed successfully."
 
- sleep 30
+sleep 20
+echo "Installing Maven..."
+
 sudo yum install -y maven
+echo "Maven installed successfully."
+
 sleep 10
+echo "Installing Docker..."
 # Install Docker dependencies
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
@@ -65,27 +79,29 @@ sleep 15
 
 # Verify Docker installation
 docker version
+echo "Docker installed successfully."
 
+echo "Installing Jenkins..."
 
 # Function to install Jenkins
-  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-  sudo yum install -y jenkins
-  sudo chown -R jenkins:jenkins /var/lib/jenkins
-  sudo chmod -R 755 /var/lib/jenkins/
-  sudo usermod -aG docker jenkins
-  sudo systemctl daemon-reload
-  sleep 15
-  sudo systemctl start jenkins
-  sudo systemctl enable jenkins
-  sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
-  sudo firewall-cmd --reload
-  sudo firewall-cmd --permanent --zone=public --add-port=8888/tcp
-  sudo firewall-cmd --reload
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install -y jenkins
+sudo chown -R jenkins:jenkins /var/lib/jenkins
+sudo chmod -R 755 /var/lib/jenkins/
+sudo usermod -aG docker jenkins
+sudo systemctl daemon-reload
+sleep 15
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+sudo firewall-cmd --reload
+sudo firewall-cmd --permanent --zone=public --add-port=8888/tcp
+sudo firewall-cmd --reload
   # Wait for Jenkins to ready
-  sleep 10
-  jenkins_version=$(sudo systemctl status jenkins | grep -oP 'Jenkins Continuous Integration Server, version \K(\d+\.\d+\.\d+)')
-  echo "Jenkins version: $jenkins_version"
+sleep 10
+jenkins_version=$(sudo systemctl status jenkins | grep -oP 'Jenkins Continuous Integration Server, version \K(\d+\.\d+\.\d+)')
+echo "Jenkins version: $jenkins_version"
   
   # Wait for Jenkins to start
 sleep 30
@@ -111,6 +127,8 @@ email=$(grep -oP 'email=\K.*' "$credentials_path")
 
 # Create admin user
 sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth admin:"$jenkins_password" create-user "$username" "$password" --full-name "$full_name" --email "$email"
+echo "Jenkins installed successfully."
+
 }
 if [[ -f /etc/redhat-release ]]; then
   install_redhat_packages
