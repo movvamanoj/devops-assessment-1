96793b9dae9a7df8dd3167c6a9af42a3d2162ee1
Manojkumar Movva <movvamanoj@gmail.com>
2023-06-09 20:16:06 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -2,105 +2,111 @@
 set -e
 set -x  # Enable verbose mode
 
+# Function to log commands and their outputs
+log_command() {
+  command="$1"
+  echo "Running command: $command"
+  eval "$command" > >(tee -a installation.log) 2>&1
+  echo "Command completed."
+}
 # Function to install Red Hat packages
 install_redhat_packages() {
 # Register the system with Red Hat subscription
-sudo subscription-manager register
+log_command "sudo subscription-manager register"
 # Install Red Hat specific packages
-sudo yum update -y
-sudo yum install -y wget unzip
+log_command "sudo yum update -y"
+log_command "sudo yum install -y wget unzip"
 # Install Java 11
 echo "Installing java11..."
-sudo yum install -y java-11-openjdk-devel
-echo "export JAVA11_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc
-source ~/.bashrc
+log_command "sudo yum install -y java-11-openjdk-devel"
+log_command "echo "export JAVA11_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc"
+log_command "source ~/.bashrc"
 echo "Installing java11 done..."
 echo "Installing java 8..."
 
 # Install Java 8
-sudo yum install -y java-1.8.0-openjdk-devel
-echo "export JAVA8_HOME=/usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
-source ~/.bashrc
+log_command "sudo yum install -y java-1.8.0-openjdk-devel"
+log_command "echo "export JAVA8_HOME=/usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc"
+log_command "source ~/.bashrc"
 echo "Installing java8 done..."
 
-echo "export DOCKER_JAVA_PATH=usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
-source ~/.bashrc
+log_command "echo "export DOCKER_JAVA_PATH=usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc"
+log_command "source ~/.bashrc"
 
 echo "Installing java17..."
 # Install Java 17
-sudo yum install -y java-17-openjdk-devel
-echo "export SONAR_JAVA_PATH=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc
-source ~/.bashrc
+log_command "sudo yum install -y java-17-openjdk-devel"
+log_command "echo "export SONAR_JAVA_PATH=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc"
+log_command "source ~/.bashrc"
 echo "Installing java17 done..."
 
 echo "Installing Git..."
 
-sudo yum install -y git
-cd ~/
-mkdir github
-cd github
-git config --global credential.helper 'cache --timeout=3600'
+log_command "sudo yum install -y git"
+log_command "mkdir github"
+log_command "cd github"
+log_command "git config --global credential.helper 'cache --timeout=3600'"
 
 # Read Git token from file
-GIT_TOKEN=$(cat "/home/movvamanoj/gitrepo/gittok.txt")
+log_command "GIT_TOKEN=$(cat "/home/movvamanoj/gitrepo/gittok.txt")"
 
 # Read username from file
-USERNAME=$(cat "/home/movvamanoj/gitrepo/gitusername.txt")
+log_command "USERNAME=$(cat "/home/movvamanoj/gitrepo/gitusername.txt")"
 
 # Clone the repository using the Git token and username for authentication
-git clone https://${USERNAME}:${GIT_TOKEN}@github.com/movvamanoj/movvaweb.git
+log_command "git clone https://${USERNAME}:${GIT_TOKEN}@github.com/movvamanoj/movvaweb.git"
 echo "Git installed successfully."
 
 sleep 20
 echo "Installing Maven..."
 
-sudo yum install -y maven
+log_command "sudo yum install -y maven"
 echo "Maven installed successfully."
 
 sleep 10
 echo "Installing Docker..."
 # Install Docker dependencies
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+log_command "sudo yum install -y yum-utils device-mapper-persistent-data lvm2"
 
 # Add Docker repository
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+log_command "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
 
 # Install Docker
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+log_command "sudo yum install -y docker-ce docker-ce-cli containerd.io"
 
 # Configure Docker environment for the current session
-sudo usermod -aG docker $USER
+log_command "sudo usermod -aG docker $USER"
 newgrp docker
 
 # Start the Docker daemon
-sudo systemctl start docker
+log_command "sudo systemctl start docker"
 
 # Enable Docker service to start on system boot
-sudo systemctl enable docker
+log_command "sudo systemctl enable docker"
 # Wait for Docker to ready
 sleep 15
 
 # Verify Docker installation
-docker version
+log_command "docker version"
 echo "Docker installed successfully."
 
 echo "Installing Jenkins..."
 
 # Function to install Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum install -y jenkins
-sudo chown -R jenkins:jenkins /var/lib/jenkins
-sudo chmod -R 755 /var/lib/jenkins/
-sudo usermod -aG docker jenkins
-sudo systemctl daemon-reload
+log_command "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo"
+log_command "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key"
+log_command "sudo yum install -y jenkins"
+log_command "sudo chown -R jenkins:jenkins /var/lib/jenkins"
+log_command "sudo chmod -R 755 /var/lib/jenkins/"
+log_command "sudo usermod -aG docker jenkins"
+log_command "sudo systemctl daemon-reload"
 sleep 15
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
-sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+log_command "sudo systemctl start jenkins"
+log_command "sudo systemctl enable jenkins"
+log_command "sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp"
 sudo firewall-cmd --reload
-sudo firewall-cmd --permanent --zone=public --add-port=8888/tcp
-sudo firewall-cmd --reload
+log_command "sudo firewall-cmd --permanent --zone=public --add-port=8888/tcp"
+log_command "sudo firewall-cmd --reload"
   # Wait for Jenkins to ready
 sleep 10
 jenkins_version=$(sudo systemctl status jenkins | grep -oP 'Jenkins Continuous Integration Server, version \K(\d+\.\d+\.\d+)')
@@ -110,13 +116,13 @@ echo "Jenkins version: $jenkins_version"
 sleep 30
 
 # Retrieve the initial one-time password
-jenkins_password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+log_command "jenkins_password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)"
 
 # Install default recommended plugins
-sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin --all
+log_command "sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin --all"
 
 # Start Jenkins
-sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ safe-restart
+log_command "sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ safe-restart"
 
 # Wait for Jenkins to restart
 sleep 30
@@ -129,7 +135,7 @@ full_name=$(grep -oP 'full_name=\K.*' "$credentials_path")
 email=$(grep -oP 'email=\K.*' "$credentials_path")
 
 # Create admin user
-sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth admin:"$jenkins_password" create-user "$username" "$password" --full-name "$full_name" --email "$email"
+log_command "sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth admin:"$jenkins_password" create-user "$username" "$password" --full-name "$full_name" --email "$email""
 echo "Jenkins installed successfully."
 
 }
