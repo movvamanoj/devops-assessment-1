6e4c96c01977c1f2ba2cd8b3151b91e5a59116fa
Manojkumar Movva <movvamanoj@gmail.com>
2023-06-10 12:31:56 +0530

    Update app.sh
    removed log

--- app.sh
+++ app.sh
@@ -5,91 +5,91 @@
 # Function to install Red Hat packages
 install_redhat_packages() {
 # Install Red Hat specific packages
-log_command "sudo yum update -y"
-log_command "sudo yum install -y wget unzip"
+sudo yum update -y
+sudo yum install -y wget unzip
 # Install Java 11
 echo "Installing java11..."
-log_command "sudo yum install -y java-11-openjdk-devel"
-log_command "echo "export JAVA11_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc"
-log_command "source ~/.bashrc"
+sudo yum install -y java-11-openjdk-devel
+echo "export JAVA11_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc
+source ~/.bashrc
 echo "Installing java11 done..."
 echo "Installing java 8..."
 
 # Install Java 8
-log_command "sudo yum install -y java-1.8.0-openjdk-devel"
-log_command "echo "export JAVA8_HOME=/usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc"
-log_command "source ~/.bashrc"
+sudo yum install -y java-1.8.0-openjdk-devel
+echo "export JAVA8_HOME=/usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
+source ~/.bashrc
 echo "Installing java8 done..."
 
-log_command "echo "export DOCKER_JAVA_PATH=usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc"
-log_command "source ~/.bashrc"
+echo "export DOCKER_JAVA_PATH=usr/lib/jvm/java-1.8.0-openjdk" >> ~/.bashrc
+source ~/.bashrc
 
 echo "Installing java17..."
 # Install Java 17
-log_command "sudo yum install -y java-17-openjdk-devel"
-log_command "echo "export SONAR_JAVA_PATH=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc"
-log_command "source ~/.bashrc"
+sudo yum install -y java-17-openjdk-devel
+echo "export SONAR_JAVA_PATH=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc
+source ~/.bashrc
 echo "Installing java17 done..."
 
 echo "Installing Git..."
 
-log_command "sudo yum install -y git"
-log_command "mkdir github"
-log_command "cd github"
-log_command "git config --global credential.helper 'cache --timeout=3600'"
+sudo yum install -y git
+mkdir github
+cd github
+git config --global credential.helper 'cache --timeout=3600'
 git clone https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/movvamanoj/movvaweb.git
 echo "Git installed successfully."
 sleep 20
 
 echo "Installing Maven..."
 
-log_command "sudo yum install -y maven"
+sudo yum install -y maven
 echo "Maven installed successfully."
 
 sleep 10
 echo "Installing Docker..."
 # Install Docker dependencies
-log_command "sudo yum install -y yum-utils device-mapper-persistent-data lvm2"
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 # Add Docker repository
-log_command "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 # Install Docker
-log_command "sudo yum install -y docker-ce docker-ce-cli containerd.io"
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 # Configure Docker environment for the current session
-log_command "sudo usermod -aG docker $USER"
+sudo usermod -aG docker $USER
 newgrp docker
 
 # Start the Docker daemon
-log_command "sudo systemctl start docker"
+sudo systemctl start docker
 
 # Enable Docker service to start on system boot
-log_command "sudo systemctl enable docker"
+sudo systemctl enable docker
 # Wait for Docker to ready
 sleep 15
 
 # Verify Docker installation
-log_command "docker version"
+docker version
 echo "Docker installed successfully."
 
 echo "Installing Jenkins..."
 
 # Function to install Jenkins
-log_command "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo"
-log_command "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key"
-log_command "sudo yum install -y jenkins"
-log_command "sudo chown -R jenkins:jenkins /var/lib/jenkins"
-log_command "sudo chmod -R 755 /var/lib/jenkins/"
-log_command "sudo usermod -aG docker jenkins"
-log_command "sudo systemctl daemon-reload"
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install -y jenkins
+sudo chown -R jenkins:jenkins /var/lib/jenkins
+sudo chmod -R 755 /var/lib/jenkins
+sudo usermod -aG docker jenkins
+sudo systemctl daemon-reload
 sleep 15
-log_command "sudo systemctl start jenkins"
-log_command "sudo systemctl enable jenkins"
-log_command "sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp"
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
 sudo firewall-cmd --reload
-log_command "sudo firewall-cmd --permanent --zone=public --add-port=8888/tcp"
-log_command "sudo firewall-cmd --reload"
+sudo firewall-cmd --permanent --zone=public --add-port=8888/tcp
+sudo firewall-cmd --reload
   # Wait for Jenkins to ready
 sleep 10
 jenkins_version=$(sudo systemctl status jenkins | grep -oP 'Jenkins Continuous Integration Server, version \K(\d+\.\d+\.\d+)')
@@ -99,13 +99,13 @@ echo "Jenkins version: $jenkins_version"
 sleep 30
 
 # Retrieve the initial one-time password
-log_command "jenkins_password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)"
+jenkins_password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 
 # Install default recommended plugins
-log_command "sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin --all"
+sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin --all
 
 # Start Jenkins
-log_command "sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ safe-restart"
+sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ safe-restart
 
 # Wait for Jenkins to restart
 sleep 30
@@ -118,7 +118,7 @@ full_name=$(grep -oP 'full_name=\K.*' "$credentials_path")
 email=$(grep -oP 'email=\K.*' "$credentials_path")
 
 # Create admin user
-log_command "sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth admin:${JENKINS_PASSWORD} create-user ${username} ${password} --full-name ${full_name} --email ${email}"
+sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ -auth admin:${JENKINS_PASSWORD} create-user ${username} ${password} --full-name ${full_name} --email ${email}
 echo "Jenkins installed successfully."
 
 
